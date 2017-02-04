c12b373701d063dfe823cb2b185d422ba3cf12ec
Ryan Walker <rawn8c@mail.umsl.edu>
2017-02-04 14:15:29 -0600

    Adds handlers and routes for all necessary pages.

--- main.py
+++ main.py
@@ -14,12 +14,68 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
 import webapp2
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *args, **kwargs):
+        self.response.out.write(*args, **kwargs)
+
+    def render_str(self,template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kwargs):
+        self.write(self.render_str(template, **kwargs))
+
+class Index(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        self.redirect("/blog")
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+class Blog(Handler):
+    def render_blog():
+        self.write("Hello!")
+
+    def get(self):
+        self.write("Hello!")
+        # self.render_blog()
+
+    def post(self):
+        pass
+        # title = self.request.get('title')
+        # art = self.request.get('art')
+
+        # if title and art:
+        #     a = Art(title=title, art=art)
+        #     a.put()
+
+        #     self.redirect("/")
+        # else:
+        #     error = "we need both a title and some artwork!"
+        #     self.render_front(title, art, error)
+
+class NewPost(Handler):
+    def render_form():
+        self.write("Hello!")
+    
+    def get(self):
+        self.write("New Post!")
+
+    def post(self):
+        self.redirect('/')
+
+class ViewPostHandler(Handler):
+    def get(self, id):
+        self.write(id)
+
+app = webapp2.WSGIApplication([('/', Index), 
+                               ('/blog', Blog), 
+                               ('/newpost', NewPost),
+                               webapp2.Route('/blog/<id:\d+>', ViewPostHandler)], 
+                               debug=True)
